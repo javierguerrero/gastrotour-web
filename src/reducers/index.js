047025ab679aeef1d62f0ac6bcfa7263ec07b8c3
jguerrero <jguerrero@goiar.com>
2020-02-11 22:45:57 -0300

    Validación para LogIn LogOut

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -10,6 +10,16 @@ const reducer = (state, action) => {
         ...state,
         mylist: state.mylist.filter((items) => items.id !== action.payload),
       };
+    case 'LOGIN_REQUEST':
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case 'LOGOUT_REQUEST':
+      return {
+        ...state,
+        user: action.payload,
+      };
     default:
       return state;
   }
